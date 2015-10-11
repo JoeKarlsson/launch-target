dfbafd094c729fab4c7678b0786ab3acf964a7e1
Alexander Anich <alexander.anich@gmail.com>
2015-10-10 19:45:32 -1000

    persistent countdown clock working

--- client/launch-timer/launch-timer.js
+++ client/launch-timer/launch-timer.js
@@ -1,6 +1,8 @@
-Template.launchTimer.rendered = function() {
-  var launchTime = this.data.lunchtime - UI._globalHelpers.thisMoment();
-  var clock = $('.clock').FlipClock(launchTime, {
-    countdown: true
-  });
-};
+Template.launchTimer.helpers({
+  makeClock: function() {
+    var launchTime = this.lunchtime - UI._globalHelpers.thisMoment();
+    var clock = $('.clock').FlipClock(launchTime, {
+      countdown: true
+    });
+  }
+});
