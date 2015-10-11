9b8ec37232b46930bd65f241b40217b7d0191288
Jesse Copeland <jesse@sudokrew.com>
2015-10-10 18:16:16 -1000

    Add views and event handlers

--- client/router.js
+++ client/router.js
@@ -1,4 +1,19 @@
 Router.configure({
     layoutTemplate: 'layout'
-  })
-  .route('/', { name: 'launchPlan' })
+  });
+Router.route('/', { name: 'initializePlan' });
+
+Router.route('/plan/:_id', {
+  name: 'launchPlan',
+  data: function () {
+    return Launches.findOne(this.params._id);
+  }
+});
+
+Router.route('/plan/:_id/select', {
+  name: 'selectPosition'
+});
+
+Router.route('/plan/:_id/targets', {
+  name: 'availableTargets'
+});
---
+++ client/views/launch-plan.js
@@ -0,0 +1,10 @@
+// Template.launchPlan.events({
+//   'click #currentPosition': function (event) {
+//     event.preventDefault();
+//     Router.go('availableTargets');
+//   },
+//   'click #pickPosition': function (event) {
+//     event.preventDefault();
+//     Router.go('selectPosition');
+//   }
+// });
